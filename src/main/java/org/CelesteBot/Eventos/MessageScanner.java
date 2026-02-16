68a39c441b7290476fd223c872e4d3ab6fae8838
S4Ktz <jaaow11@gmail.com>
2026-02-16 15:40:55 -0300

    Atualizando lista de palavras bloqueadas e estilizando a mensagem enviada pelo bot

--- src/main/java/org/CelesteBot/Eventos/MessageScanner.java
+++ src/main/java/org/CelesteBot/Eventos/MessageScanner.java
@@ -1,39 +1,70 @@
 package org.CelesteBot.Eventos;
 
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Array;
+import java.awt.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MessageScanner extends ListenerAdapter {
-        //lista de mensagens que o bot tem que bloquear
-       private final List<String> mensagensBloqueadas = Arrays.asList(
-               "Gordo",
-               "Preto",
-               "Macaco",
-               "Gorda",
-               "Imenso"
+        //lista de mensagens que o "bot" tem que bloquear
+       private final List<String> mensagensBloqueadas;
 
-       );
+    public MessageScanner() {
+        this.mensagensBloqueadas = new ArrayList<>();
+        mensagensBloqueadas.add("gordo");
+        mensagensBloqueadas.add("preto");
+        mensagensBloqueadas.add("macaco");
+        mensagensBloqueadas.add("puta");
+        mensagensBloqueadas.add("filha da puta");
+        mensagensBloqueadas.add("gorda");
+        mensagensBloqueadas.add("imensa");
+        mensagensBloqueadas.add("imenso");
+        mensagensBloqueadas.add("escuridão");
+        mensagensBloqueadas.add("baleia");
+        mensagensBloqueadas.add("vagabunda");
+        mensagensBloqueadas.add("macaca");
+        mensagensBloqueadas.add("viado");
+        mensagensBloqueadas.add("viadinho");
+        mensagensBloqueadas.add("negresco");
+        mensagensBloqueadas.add("petróleo");
+        mensagensBloqueadas.add("vadia");
+        mensagensBloqueadas.add("");
+
+    }
 
 
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         if (event.getAuthor().isBot()) return; //impedir o boot de ficar em um loop infinito de mensagens
+
         String MensagemLida = event.getMessage().getContentRaw();
+        String CANAL_DE_LOG = "1473012450570801365";
 
         for (String palavra : mensagensBloqueadas){
-            if (MensagemLida.contains(palavra)){
-                //bot tem que analisar a mensagem e deletar ela e enviar uma mensagem de aviso para o usuario
-                //pegar o canal em que a mensagem foi enviada e enviar no mesmo canal a mensagem de aviso
+            if (MensagemLida.toLowerCase().contains(palavra)){
+
+                EmbedBuilder embedBuilder = new EmbedBuilder();
+                embedBuilder.setTitle("⚠ Mensagem suspeita detectada ⚠");
+                embedBuilder.setColor(Color.red);
+                embedBuilder.addField("Usuario: ", event.getAuthor().getAsMention(),true);
+                embedBuilder.addField("Canal: ",event.getChannel().getAsMention(),true);
+                embedBuilder.addField("Palavra:","||"+ palavra +"||",true );
+                embedBuilder.addField("Mensagem: ",MensagemLida,false);
+                embedBuilder.setFooter("ID DO USUARIO: "+ event.getAuthor().getId());
+
+                TextChannel CanalDeLog = event.getGuild().getTextChannelById(CANAL_DE_LOG);
                 event.getMessage().delete().queue();
-                event.getChannel().sendMessage("Mensagem Impropria detectada!").queue();
+
+                if (CanalDeLog != null) {
+                    CanalDeLog.sendMessageEmbeds(embedBuilder.build()).queue();
+                }
 
                 break;
             }
