986fe66015bcb7f1e44652a47c91a2f9a76bb6a8
S4Ktz <jaaow11@gmail.com>
2026-02-20 16:22:04 -0300

    Update CelesteScanBot.java

--- src/main/java/org/CelesteBot/CelesteScanBot.java
+++ src/main/java/org/CelesteBot/CelesteScanBot.java
@@ -2,11 +2,15 @@ package org.CelesteBot;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
+import org.CelesteBot.Comandos.SetChannel;
 import org.CelesteBot.Eventos.MessageScanner;
 
 import javax.security.auth.login.LoginException;
@@ -16,6 +20,7 @@ public class CelesteScanBot {
 
     private final Dotenv config;
     private final ShardManager shardManager;
+    public static long canalReportID = 0L;
 
     public CelesteScanBot() throws LoginException {
         config = Dotenv.configure().load();
@@ -29,13 +34,30 @@ public class CelesteScanBot {
 
 
 
-        //Registar eventos
+
+
+
+
+
+
+        //Registar eventos/comandos
         shardManager.addEventListener(new MessageScanner());
+        shardManager.addEventListener(new SetChannel());
+
+        //adicionar comandos
+        shardManager.getShards().get(0).upsertCommand("setchannel","Seleciona o canal de reports")
+                .addOption(OptionType.CHANNEL,"canal","selecione o canal",true)
+                .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR)).queue();
+
+
+
+
 
     }
 
     public ShardManager getShardManager(){
         return shardManager;
+
     }
 
     public static void main(String[] args) {
