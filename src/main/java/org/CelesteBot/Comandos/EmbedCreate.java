7087907fcac9d5fbc857821e8c61c8db6c1e6be3
S4Ktz <jaaow11@gmail.com>
2026-05-11 19:05:00 -0300

    Update EmbedCreate.java

--- src/main/java/org/CelesteBot/Comandos/EmbedCreate.java
+++ src/main/java/org/CelesteBot/Comandos/EmbedCreate.java
@@ -1,5 +1,7 @@
 package org.CelesteBot.Comandos;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
@@ -9,33 +11,73 @@ import net.dv8tion.jda.api.interactions.modals.Modal;
 import org.jetbrains.annotations.NotNull;
 import org.w3c.dom.Text;
 
+import java.awt.*;
+import java.util.Objects;
+
 public class EmbedCreate extends ListenerAdapter {
+
+    @Override
+    public void onModalInteraction(@NotNull ModalInteractionEvent event) {
+        if (!event.getModalId().equals("gerador-embed")){
+
+            String titulo = Objects.requireNonNull(event.getValue("embed-titulo")).toString();
+
+            String corHex = Objects.requireNonNull(event.getValue("embed-cor")).toString();
+
+            String desc = Objects.requireNonNull(event.getValue("embed-desc")).toString();
+
+            String imagemUrl = Objects.requireNonNull(event.getValue("embed-imagem")).toString();
+
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder.setTitle(titulo);
+            embedBuilder.setDescription(desc);
+
+            try {
+                embedBuilder.setColor(Color.decode(corHex.startsWith("#") ? corHex : "#" + corHex));
+            }catch (Exception e){
+                embedBuilder.setColor(Color.WHITE);
+            }
+
+            if (imagemUrl != null && imagemUrl.startsWith("http")){
+                embedBuilder.setImage(imagemUrl);
+            }
+
+            event.replyEmbeds(embedBuilder.build()).queue();
+
+    }
+    }
+
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         if (!event.getName().equals("embed")){
-            TextInput titulo = TextInput.create("Embed-titulo","titulo", TextInputStyle.SHORT)
+            TextInput titulo = TextInput.create("embed-titulo","titulo", TextInputStyle.SHORT)
                     .setPlaceholder("coloque o titulo")
                     .setRequired(true)
                     .build();
 
-            TextInput cor = TextInput.create("Embed-cor","Cor (Ex:#1234567890)",TextInputStyle.SHORT)
+            TextInput cor = TextInput.create("embed-cor","Cor (Ex:#1234567890)",TextInputStyle.SHORT)
                     .setPlaceholder("#")
                     .setRequired(false)
                     .build();
 
-            TextInput descricao = TextInput.create("Embed-desc","descrição",TextInputStyle.PARAGRAPH)
+            TextInput descricao = TextInput.create("embed-desc","descrição",TextInputStyle.PARAGRAPH)
                     .setPlaceholder("coloque a descrição")
                     .setRequired(true)
                     .build();
 
-            TextInput imagem = TextInput.create("Embed-imagem","Imagem",TextInputStyle.PARAGRAPH)
+            TextInput imagem = TextInput.create("embed-imagem","Imagem",TextInputStyle.PARAGRAPH)
                     .setPlaceholder("Coloque o link da imagem")
                     .setRequired(false)
                     .build();
 
-            Modal modal = Modal.create("Gerador-embed","criar embed")
-                    .addComponents(ActionRow.of(titulo),ActionRow.of(cor),)
+            Modal modal = Modal.create("gerador-embed","criar embed")
+                    .addComponents(ActionRow.of(titulo),ActionRow.of(cor),ActionRow.of(descricao),ActionRow.of(imagem))
+                    .build();
         }
+
+
+
+
     }
 }
